0350586897fd49d14c055d87e4f25f47cb4d1913
MITECH\erica <erica@chameleonpower.com>
2018-03-05 15:55:45 -0500

    Finished section 2 exercises

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> Customers = new List<Customer>
+        {
+            new Customer() { Id = 1, Name = "Joe" },
+            new Customer() { Id = 2, Name = "Mama" }
+        };
+        // GET: Customers
+        public ActionResult Index()
+        {   
+            return View(Customers);
+        }
+        public ActionResult Details(string id)
+        {
+            try
+            {
+                var customer = Customers.First(item => item.Id == int.Parse(id));
+                return View(customer);
+            }
+            catch
+            {
+                throw new HttpException(404, "No customer.");
+            }
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private List<Movie> Movies = new List<Movie>
+        {
+            new Movie() {Id = 0, Name = "The Big Lebowski"},
+            new Movie() {Id = 1, Name = "The Big Lebowski 2: 2 Big 2 Lebowski"},
+            new Movie() {Id = 2, Name = "The Big Lebowski 3: Tokyo Drift"}
+        };
+        public ActionResult Index()
+        {
+            return View(Movies);
+        }
+
+        public ActionResult Details(string id)
+        {
+            try
+            {
+                var movie = Movies.First(item => item.Id == int.Parse(id));
+                return View(movie);
+            }
+            catch
+            {
+                throw new HttpException(404, "No movie.");
+            }
+        }
+    }
+}
